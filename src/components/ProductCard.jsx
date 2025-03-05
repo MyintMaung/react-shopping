6e1e6e41cacd17ebce8487a3c0e0d1cd2b28f6f6
MyintMaung <myintmaungmaung845@gamil.com>
2025-03-05 11:31:14 +0630

    rating update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Rating from "./Rating";
 
 const ProductCard = ({
   product: {
@@ -12,7 +13,8 @@ const ProductCard = ({
     <div className="border border-black p-5 flex flex-col items-start gap-5">
       <img src={image} className="h-40" alt="" />
       <p className="font-bold line-clamp-2">{title}</p>
-      <div className="flex justify-between items-end w-full mt-auto">
+      <Rating rate={rate} />
+      <div className="flex justify-between items-end w-full">
         <p>${price}</p>
         <button className="border border-black text-xs px-2 py-1">
           Add Cart
